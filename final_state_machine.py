ac3035c51d391d42d7a975cecd4b5e200ee74375
Sverre Rynning-Tønnesen <Sverre@dhcp-10-24-6-136.wlan.ntnu.no>
2019-10-10 12:57:11 +0200

    working?

--- final_state_machine.py
+++ final_state_machine.py
@@ -2,5 +2,24 @@
 
 class FSM:
 
+    def __init__(self):
+        self.rule_list = [] #list of rules
+
     def add_rule(self, r):
-        rule = r
+        '''Add rule to rule list'''
+        self.rule_list.append(r)
+
+    def get_next_signal(self):
+        '''Query the agent for the next signal'''
+
+    def run_rules(self):
+        '''Go through the rule set, in order, applying each rule until one of the rules is fired'''
+
+    def apply_rule(self):
+        '''Check whether the conditions of a rule are met'''
+
+    def fire_rule(self):
+        '''Use the consequent of a rule to a) set the next state of the FSM, and b) call the appropriate agent action method'''
+
+    def main_loop(self):
+        '''Use the consequent of a rule to a) set the next state of the FSM, and b) call the appropriate agent action method'''

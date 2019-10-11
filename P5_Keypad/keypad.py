a5487b96a625475ffde561f500ec6ec25a91e593
olestole <olestole@outlook.com>
2019-10-11 12:47:20 +0200

    RPI

--- P5_Keypad/keypad.py
+++ P5_Keypad/keypad.py
@@ -1,10 +1,16 @@
 '''Keypad class, with functions'''
 
+import RPi.GPIO as GPIO
+
+
 class Keypad:
 
     def setup(self):
         '''Set the proper mode via: GPIO.setmode(GPIO.BCM). Also, use GPIO functions to set the row
          pins as outputs and the column pins as inputs'''
+
+
+
 
     def do_polling(self):
         '''Use nested loops (discussed above) to determine the key currently being pressed on the keypad'''

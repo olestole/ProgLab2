35496e840841486ff80827d82f8dc1e9a26a4095
Henrik Gundersen <henrikg@dhcp-10-22-8-81.wlan.ntnu.no>
2019-10-14 16:58:10 +0200

    push sverre

--- P5_Keypad/keypad_controller.py
+++ P5_Keypad/keypad_controller.py
@@ -4,29 +4,40 @@ from led_board import Led_board
 
 class KPC:
 
-    def __init__(self, p_keypad, p_ledboard, p_name, o_signal):
+    def __init__(self, p_keypad, p_ledboard, p_name, password):
         self.pointer_keypad = p_keypad
         self.pointer_ledboard = p_ledboard
         '''a few simple strings or arrays for holding important sequences of keystrokes, such as a passcode-buffer for all numbers in an ongoing password-entry attempt'''
         self.password_buffer = ""
+        self.password = password
         self.pathname = p_name  # the complete pathname to the file holding the KPC’s password
-        self.override_signal = o_signal
+        self.override_signal = ""
         '''slotsforholdingtheLEDid(Lid)andlightingduration(Ldur)–bothenteredviathekeypad – so that it can initiate the action of turning a specific LED on for a specific length of time'''
 
     def init_passcode_entry(self):
         '''Clear the passcode-buffer and initiate a ”power up” lighting sequence on the LED Board. This should be done when the user first presses the keypad'''
+        self.password_buffer = ""
         self.pointer_ledboard.power_up()
 
     def get_next_signal(self):
         '''Return the override-signal, if it is non-blank; otherwise query the keypad for the next pressed key'''
-        return self.pointer_keypad.get_next_signal()
+        if self.override_signal:
+            return self.override_signal
+        else:
+            self.pointer_keypad.get_next_signal()
 
     def verify_login(self):
         '''Check that the password just entered via the keypad matches that in the pass- word file.
         Store the result (Y or N) in the override-signal. Also, this should call the LED Board to initiate the appropriate
          lighting pattern for login success or failure'''
+        if self.password_buffer == self.password:
+            self.twinkle_leds(3)
+            self.override_signal = "Y"
+        else:
+            self.flash_leds(3)
+            self.override_signal = "N"
 
-    def validate_passcode_change(self):
+    def validate_passcode_change(self, new_passw):
         '''Check that the new password is legal. If so, write the new pass- word in the password file.
         A legal password should be at least 4 digits long and should contain no symbols other than the digits 0-9.
         As in verify login, this should use the LED Board to signal success or failure in changing the password'''
@@ -53,7 +64,7 @@ def main():
     qpad.setup()
     ledboard = Led_board()
     ledboard.setup()
-    kpc = KPC(qpad, ledboard, "txt", "passw")
+    kpc = KPC(qpad, ledboard, "filepath", "1234")
     kpc.flash_leds(5)
 
 

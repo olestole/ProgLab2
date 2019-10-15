0882dd184c504f855856cc03cbc2073edaac4030
Sverre Rynning-Tønnesen <Sverre@dhcp-10-24-6-121.wlan.ntnu.no>
2019-10-15 11:44:42 +0200

    pylint

--- P5_Keypad/final_state_machine.py
+++ P5_Keypad/final_state_machine.py
@@ -1,10 +1,10 @@
-'''Finite State Machine class, with functions'''
+"""Finite State Machine class, with functions"""
 
 from keypad_controller import KPC
 from keypad import Keypad
 from led_board import Led_board
 import time
-import keypad_controller
+
 
 class FSM:
 
@@ -21,78 +21,55 @@ class FSM:
         self.led_board.setup()
 
         self.curr_state = "s_init"
-        self.rule_list = [] #list of rules
+        self.rule_list = []  # list of rules
         self.curr_signal = None
 
         self.rules_need_input = []
 
     def add_rules(self):
-        '''Add rule to rule list'''
+        """Add rule to rule list"""
 
-        '''Password rules'''
-        a1 = Rule('s_init', 's_read', 'all_symbols', self.agent.init_passcode_entry)
-        a2 = Rule('s_read', 's_read', 'all_digits', self.agent.append_next_password_digit) #ta inn input
-        a3 = Rule('s_read', 's_verify', '*', self.agent.verify_password)
-        a4 = Rule('s_read', 's_init', 'all_symbols', self.agent.reset_agent)
-        a5 = Rule('s_verify', 's_active', 'Y', self.agent.fully_activate_agent)
-        a6 = Rule('s_verify', 's_init', 'N', self.agent.reset_agent)
-        a11 = Rule('s_active', 's_read_2', '*', self.agent.init_passcode_entry)
-        a21 = Rule('s_read_2', 's_read_2', 'all_digits', self.agent.append_next_password_digit) #ta inn input
-        a7 = Rule('s_read_2', 's_read_3', '*', self.agent.cache_new_password)
-        a61 = Rule('s_read_2', 's_active', 'all_symbols', self.agent.refresh_agent)
-        a22 = Rule('s_read_3', 's_read_3', 'all_digits', self.agent.append_next_password_digit_old)#ta inn input
-        a8 = Rule('s_read_3', 's_active', '*', self.agent.compare_new_passwords)
-        a62 = Rule('s_read_3', 's_active', 'all_symbols', self.agent.refresh_agent)
+        """Password rules"""
+        rule_a1 = Rule('s_init', 's_read', 'all_symbols', self.agent.init_passcode_entry)
+        rule_a2 = Rule('s_read', 's_read', 'all_digits', self.agent.append_next_password_digit)  # ta inn input
+        rule_a3 = Rule('s_read', 's_verify', '*', self.agent.verify_password)
+        rule_a4 = Rule('s_read', 's_init', 'all_symbols', self.agent.reset_agent)
+        rule_a5 = Rule('s_verify', 's_active', 'Y', self.agent.fully_activate_agent)
+        rule_a6 = Rule('s_verify', 's_init', 'N', self.agent.reset_agent)
+        rule_a11 = Rule('s_active', 's_read_2', '*', self.agent.init_passcode_entry)
+        rule_a21 = Rule('s_read_2', 's_read_2', 'all_digits', self.agent.append_next_password_digit)  # ta inn input
+        rule_a7 = Rule('s_read_2', 's_read_3', '*', self.agent.cache_new_password)
+        rule_a61 = Rule('s_read_2', 's_active', 'all_symbols', self.agent.refresh_agent)
+        rule_a22 = Rule('s_read_3', 's_read_3', 'all_digits', self.agent.append_next_password_digit_old)  # ta inn input
+        rule_a8 = Rule('s_read_3', 's_active', '*', self.agent.compare_new_passwords)
+        rule_a62 = Rule('s_read_3', 's_active', 'all_symbols', self.agent.refresh_agent)
 
-        '''Light rules'''
-        s1 = Rule('s_active', 's_led', '0-5_digits', self.agent.choose_led)#ta inn input
-        s2 = Rule('s_led', 's_time', '*', self.agent.begin_duration_entry)
-        s3 = Rule('s_time', 's_time', 'all_digits', self.agent.choose_duration)#ta inn input
-        s4 = Rule('s_time', 's_active', '*', self.agent.complete_duration)
-        s5 = Rule('s_active', 's_logout', '#', self.agent.begin_logout)
-        s6 = Rule('s_logout', 's_done', '#', self.agent.exit_action)
+        """Light rules"""
+        rule_s1 = Rule('s_active', 's_led', '0-5_digits', self.agent.choose_led)  # ta inn input
+        rule_s2 = Rule('s_led', 's_time', '*', self.agent.begin_duration_entry)
+        rule_s3 = Rule('s_time', 's_time', 'all_digits', self.agent.choose_duration)  # ta inn input
+        rule_s4 = Rule('s_time', 's_active', '*', self.agent.complete_duration)
+        rule_s5 = Rule('s_active', 's_logout', '#', self.agent.begin_logout)
+        rule_s6 = Rule('s_logout', 's_done', '#', self.agent.exit_action)
 
-        # Forslag til å endre 189418 append kall
-        # self.rule_list.append([a1, a2, a3, a4, a5, a6, a11, a21, a7, a61, a22, a8, a62, s1, s2, s3, s4, s5, s6])
+        """Add rules to rule_list"""
+        self.rule_list.append([rule_a1, rule_a2, rule_a3, rule_a4, rule_a5, rule_a6, rule_a11, rule_a21, rule_a7,
+                               rule_a61, rule_a22, rule_a8, rule_a62, rule_s1, rule_s2, rule_s3, rule_s4, rule_s5,
+                               rule_s6])
 
-        self.rule_list.append(a1)
-        self.rule_list.append(a2)
-        self.rule_list.append(a3)
-        self.rule_list.append(a4)
-        self.rule_list.append(a5)
-        self.rule_list.append(a6)
-        self.rule_list.append(a11)
-        self.rule_list.append(a21)
-        self.rule_list.append(a7)
-        self.rule_list.append(a61)
-        self.rule_list.append(a22)
-        self.rule_list.append(a8)
-        self.rule_list.append(a62)
-
-        self.rule_list.append(s1)
-        self.rule_list.append(s2)
-        self.rule_list.append(s3)
-        self.rule_list.append(s4)
-        self.rule_list.append(s5)
-        self.rule_list.append(s6)
-
-        '''Add to need input list'''
-        self.rules_need_input.append(a2)
-        self.rules_need_input.append(a21)
-        self.rules_need_input.append(a22)
-        self.rules_need_input.append(s1)
-        self.rules_need_input.append(s3)
-
+        """Add to need input list"""
+        self.rules_need_input.append([rule_a2, rule_a21, rule_a22, rule_s1, rule_s3])
 
     def get_next_signal(self):
-        '''Query the agent for the next signal'''
+        """Query the agent for the next signal"""
         self.curr_signal = self.agent.get_next_signal()
 
     def run_rules(self):
-        '''Go through the rule set, in order, applying each rule until one of the rules is fired'''
+        """Go through the rule set, in order, applying each rule until one of the rules is fired"""
         for i in self.rule_list:
             if i.state1 == self.curr_state and self.curr_signal in i.legal_signals:
-                print("curr_state:", self.curr_state, "\ti.state1:", i.state1, "\ti.legal_signals:", i.legal_signals, "\tcurr_signal:", self.curr_signal)
+                print("curr_state:", self.curr_state, "\ti.state1:",
+                      i.state1, "\ti.legal_signals:", i.legal_signals, "\tcurr_signal:", self.curr_signal)
                 print("FOUND RULE")
                 self.curr_state = i.state2
                 if i in self.rules_need_input:
@@ -103,14 +80,16 @@ class FSM:
 
 
     def apply_rule(self):
-        '''Check whether the conditions of a rule are met'''
+        """Check whether the conditions of a rule are met"""
 
     def fire_rule(self):
-        '''Use the consequent of a rule to a) set the next state of the FSM, and b) call the appropriate agent action method'''
+        """Use the consequent of a rule to a) set the next state of the FSM,
+         and b) call the appropriate agent action method"""
 
 
     def main_loop(self):
-        '''Use the consequent of a rule to a) set the next state of the FSM, and b) call the appropriate agent action method'''
+        """Use the consequent of a rule to a) set the next
+        state of the FSM, and b) call the appropriate agent action method"""
         while self.curr_state != 's_done':
             print('i')
             self.get_next_signal()
@@ -121,8 +100,8 @@ class FSM:
             time.sleep(0.5)
 
 
-
 class Rule:
+    """Rule class"""
 
     def __init__(self, state1, state2, signal, action):
         self.state1 = state1
@@ -131,23 +110,23 @@ class Rule:
         elif signal == 'all_digits':
             self.legal_signals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         elif signal == '0-5_digits':
-            self.legal_signals = [0,1,2,3,4,5]
+            self.legal_signals = [0, 1, 2, 3, 4, 5]
         else:
             self.legal_signals = [signal]
         self.state2 = state2
         self.action = action
 
+
 def main():
+    """Main function"""
+
     print("START")
     keypad = Keypad()
     led_board = Led_board()
-    kpc = KPC(keypad, led_board, "path", "6969")
+    kpc = KPC(keypad, led_board, "6969")
     fsm = FSM(kpc, keypad, led_board)
-    print("for")
     fsm.add_rules()
-    print("etter")
     fsm.main_loop()
-
 
 
 if __name__ == "__main__":

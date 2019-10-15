1cef6b133ae16ae6abf376f3c7761da82db0c6f2
olestole <olestole@outlook.com>
2019-10-15 11:06:19 +0200

    test3

--- P5_Keypad/final_state_machine.py
+++ P5_Keypad/final_state_machine.py
@@ -91,12 +91,8 @@ class FSM:
     def run_rules(self):
         '''Go through the rule set, in order, applying each rule until one of the rules is fired'''
         for i in self.rule_list:
-            print(self.curr_signal)
-            print(i.legal_signals)
-
-
             if i.state1 == self.curr_state and self.curr_signal in i.legal_signals:
-                print("curr_state:", self.curr_state, "\ti.state1:", i.state1, "\ti.legal_signals:", i.legal_signals)
+                print("curr_state:", self.curr_state, "\ti.state1:", i.state1, "\ti.legal_signals:", i.legal_signals, "\tcurr_signal:", self.curr_signal)
                 print("FOUND RULE")
                 self.curr_state = i.state2
                 if i in self.rules_need_input:

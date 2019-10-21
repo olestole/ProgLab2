73df572a90a8f8d21cf764513eef8dfc29fc9845
Ola Hermann Opheim <olahop@stud.ntnu.no>
2019-10-21 13:59:28 +0200

    testrun

--- P6_Robot/Help_Classes/robodemo.py
+++ P6_Robot/Help_Classes/robodemo.py
@@ -4,7 +4,7 @@ from time import sleep
 import random
 #import imager2 as IMR
 from reflectance_sensors import ReflectanceSensors
-from camera import Camera
+#from camera import Camera
 from motors import Motors
 from ultrasonic import Ultrasonic
 from zumo_button import ZumoButton

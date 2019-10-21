38258c72938bb8dc3dfa14941912a03239d6aeed
Sverre Rynning-Tønnesen <Sverre@dhcp-10-24-4-194.wlan.ntnu.no>
2019-10-21 15:46:11 +0200

    updated functions

--- P6_Robot/motob.py
+++ P6_Robot/motob.py
@@ -6,9 +6,12 @@ class Motob:
         self.motors = [] #list of the motors whose settings will be determined by the motob
         self.value = value #a holder of the most recent motor recommendation sent to the motob
 
-    def update(self):
+    def update(self, motor_recommendations):
         """receive a new motor recommendation, load it into the value slot, and operationalize it"""
+        self.value = motor_recommendations
+        self.operationalize()
 
     def operationalize(self):
         """convert a motor recommendation into one or more motor settings, which are sent to the corresponding motor(s)"""
-
+        for motor in self.motors:
+            motor.set_value(self.value)

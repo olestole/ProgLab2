a4ffe62b99b58537fb1f2ac4b4069b50555d69b4
Ola Hermann Opheim <olahop@stud.ntnu.no>
2019-10-21 15:31:17 +0200

    sonsob fix

--- P6_Robot/sensob.py
+++ P6_Robot/sensob.py
@@ -13,7 +13,7 @@ class Sensob:
 
     def update(self):
         """force the sensob to fetch the relevant sensor value(s) and convert them into the pre-processed sensob value"""
-        self.value = self.sensor.sensor_get_value()
+        self.value = self.sensor.get_value()
 
     def get_value(self):
         """ Get value from sensor wrapper """

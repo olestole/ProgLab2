cd5c83105aa7e777aa127381d8df429e85fe24b5
Ola Hermann Opheim <olahop@stud.ntnu.no>
2019-10-21 12:58:13 +0200

    Spcified some methods in bbcon

--- P6_Robot/arbitrator.py
+++ P6_Robot/arbitrator.py
@@ -1,10 +1,13 @@
+""" Validate  """
 
 
 class Arbitrator:
 
-    def __init__(self, i_variable):
-        self.instance_variable = i_variable # a pointer to the bbcon
+    instance_variable = None   # a pointer to the bbcon
 
-    def choose_action(self):
-        """check all of the active behaviors and pick a winner"""
+    def __init__(self, i_variable):
+        self.instance_variable = i_variable
+
+    def choose_action(self, active_behaviors):
+        """check all of the active behaviors and return a winner"""
 
--- P6_Robot/bbcon.py
+++ P6_Robot/bbcon.py
@@ -1,26 +1,41 @@
 """The highest-level class, BBCON (Behavior-Based Controller)"""
 
+import time
+from arbitrator import Arbitrator
 
 class BBCON:
 
+    behaviors = []
+    active_behaviors = []
+    sensobs = []
+    motobs = []
+    arbitrator = None
+
+
     def __init__(self, arbitrator):
-        self.behaviors = []
-        self.active_behaviors = []
-        self.sensobs = []
-        self.motobs = []
-        self.arbitrator = arbitrator
+        self.arbitrator = Arbitrator(self)
 
-    def add_behavior(self):
+    def add_behavior(self, behavior):
         """append a newly-created behavior onto the behaviors list"""
+        self.behaviors.append(behavior)
 
-    def add_sensob(self):
+    def add_sensob(self, sensor):
         """append a newly-created sensob onto the sensobs list"""
+        self.sensobs.append(sensor)
 
-    def activate_behavior(self):
+    def activate_behavior(self, behavior):
         """add an existing behavior onto the active-behaviors list"""
+        self.activate_behavior.append(behavior)
 
-    def deactive_behavior(self):
+    def deactive_behavior(self, behavior):
         """remove an existing behavior from the active behaviors list"""
+        self.activate_behavior.remove(behavior)
 
     def run_one_timestep(self):
         """constitutes the core BBCON activity"""
+        #TODO: Update all sensobs
+        #TODO: Update all behaviors
+        arbitrator.choose_action(self.active_behaviors)
+        #TODO: Update the motobs based on these motor recommendations
+        time.sleep(0.5)
+        #TODO: Reset the sensobs

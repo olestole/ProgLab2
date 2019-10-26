a939f304db0b3aeeb1fa70460dee59c787f98f47
Ola Hermann Opheim <olahop@stud.ntnu.no>
2019-10-26 15:57:26 +0200

    a

--- P6_Robot/behavior.py
+++ P6_Robot/behavior.py
@@ -57,12 +57,13 @@ class Behavior:
                     if min(sensor.get_value()) > self.sensor_limits[sensor_count]:
                         all_active_values = False
                         break
-                elif sensor_count == 2: 
-                    color = self.sensor_limits[sensor_count].index(170)
-                    if sensor.get_value()[color] < self.sensor_limits[sensor_count][color]:
-                        print("color no good, ", color)
-                        all_active_values = False
-                        break
+                elif sensor_count == 2:
+                    if self.sensor_limits[sensor_count].index(170):
+                        color = self.sensor_limits[sensor_count].index(170)
+                        if sensor.get_value()[color] < self.sensor_limits[sensor_count][color]:
+                            print("color no good, ", color)
+                            all_active_values = False
+                            break
                 sensor_count += 1
             if not all_active_values:
                 self.active_flag = False

8a7c4c9600f165ae48ef6094ef6972bfef4322e8
Ola Hermann Opheim <olahop@stud.ntnu.no>
2019-10-26 10:44:17 +0200

    a

--- P6_Robot/sensob.py
+++ P6_Robot/sensob.py
@@ -1,7 +1,7 @@
 import sys
 import os
-from ProgLab2.P6_Robot.Help_Classes.camera import Camera
-from P6_Robot.Help_Classes.imager2 import Imager
+from Help_Classes.camera import Camera
+from Help_Classes.imager2 import Imager
 
 class Sensob:
 
